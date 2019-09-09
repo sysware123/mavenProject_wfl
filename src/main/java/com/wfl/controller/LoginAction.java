bda145507b693e4386544b695341d4f331194008
wangfl <wangfl@sysware.com.cn>
2019-09-09 16:48:56 +0800

    Initial commit

---
+++ src/main/java/com/wfl/controller/LoginAction.java
@@ -0,0 +1,18 @@
+package com.wfl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author SPC-044
+ * @date 16:16
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginAction {
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "index";
+    }
+}
